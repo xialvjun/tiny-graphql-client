f26d4f68fc14ceef0f221cd63a8b471bc708ca48
xialvjun <xialvjun@live.com>
2018-05-31 18:40:11 +0800

    my standard ts lib

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export declare function create_client<T>(send: (body: {
-    operationName: string;
-    query: string;
-    variables?: any;
-}, extra?: any) => T): {
-    run: (query: string, variables?: any, extra?: any) => T;
-    register_fragment: (fragment: string) => void;
-    fragments: {};
-};
